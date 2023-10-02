ee0368e3a3b1258d7379e72e2b3727aca9ac593a
Dave Everitt <dave.everitt@icloud.com>
2023-10-02 10:25:29 +0200

    cleanu[ before querySelectorAll

--- assets/scripts/id_handler.js
+++ assets/scripts/id_handler.js
@@ -8,3 +8,29 @@ test.addEventListener("click", (e)=> {
     behavior: 'smooth'
   })
 })
+
+/*
+document.querySelectorAll('.input')
+  .forEach(input => input.addEventListener('focus', this.onInputFocus));
+
+---
+document.querySelectorAll('foo').forEach(item => {
+  THE_THING_TO_DO
+});
+
+---
+const doOnClick = (e) => {
+    // code here
+}
+document.querySelectorAll('.jump').forEach((el) => {
+  el.addEventListener('click', doOnClick)
+})
+
+---
+document.querySelectorAll('.some-class').forEach(item => {
+  item.addEventListener('click', event => {
+    //handle click
+  })
+})
+
+*/
