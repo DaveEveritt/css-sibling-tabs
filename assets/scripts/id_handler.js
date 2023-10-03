819381ef92d10e9dd22cc0d6ecef5d93f880deb4
Dave Everitt <dave.everitt@icloud.com>
2023-10-03 17:03:07 +0200

    finish jump link handler

--- assets/scripts/id_handler.js
+++ assets/scripts/id_handler.js
@@ -2,13 +2,6 @@
 
 let theTarget;
 
-// test.addEventListener("click", (e)=> {
-//   theTarget = e.target.attributes["data-link"].value;
-//   document.getElementById(theTarget).scrollIntoView({
-//     behavior: 'smooth'
-//   })
-// })
-
 const jumpTo = (e) => {
   theTarget = e.target.attributes["data-link"].value;
   document.getElementById(theTarget).scrollIntoView({
@@ -20,16 +13,3 @@ const jumpLinks = [...document.querySelectorAll('[data-link]')];
 jumpLinks.map((el) => {
   el.addEventListener('click', jumpTo)
 })
-
-/*
-get all ".toplinkheader"
-querySelectorAll(data-link[""])
-querySelectorAll(href["#rulerships"])
-
-OR MAP (FASTER) - FAILS
-
-document.querySelectorAll('.jump').map((el) => {
-  el.addEventListener('click', jumpTo)
-})
-
-*/
