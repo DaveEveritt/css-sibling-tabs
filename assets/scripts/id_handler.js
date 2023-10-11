60113c5e8d03e1e128e9ab7a523b9c70233fd856
Dave Everitt <dave.everitt@icloud.com>
2023-10-11 22:48:58 +0200

    make all content and code neutral

--- assets/scripts/id_handler.js
+++ assets/scripts/id_handler.js
@@ -4,10 +4,12 @@ let theTarget;
 
 const jumpTo = (e) => {
   theTarget = e.target.attributes["data-link"].value;
-  document.getElementById(theTarget).scrollIntoView({
-    behavior: 'smooth'
-  })
+  document.getElementById(theTarget).scrollIntoView();
 }
+//   IF SMOOTH SCROLL NOT HANDLED BY CSS AND BROWSERS:
+//   document.getElementById(theTarget).scrollIntoView({
+//     behavior: 'smooth'
+//   })
 
 const jumpLinks = [...document.querySelectorAll('[data-link]')];
 jumpLinks.map((el) => {
