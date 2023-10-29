821d7a7b8b4476b6e2e457340943916cb80f595e
Dave Everitt <dave.everitt@icloud.com>
2023-10-29 12:56:05 +0000

    remove redundant IDs and clclasses

--- assets/scripts/id_handler.js
+++ assets/scripts/id_handler.js
@@ -6,10 +6,8 @@ const jumpTo = (e) => {
   theTarget = e.target.attributes["data-link"].value;
   document.getElementById(theTarget).scrollIntoView();
 }
-//   IF SMOOTH SCROLL NOT HANDLED BY CSS AND BROWSERS:
-//   document.getElementById(theTarget).scrollIntoView({
-//     behavior: 'smooth'
-//   })
+// add `behaviour` if smooth not handled by CSS/browser:
+//   [...].scrollIntoView({behavior: 'smooth'})
 
 const jumpLinks = [...document.querySelectorAll('[data-link]')];
 jumpLinks.map((el) => {
